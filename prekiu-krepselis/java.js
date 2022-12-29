3470033bcda3156b40eafff8ae17ade5b18b6501
lpovy <102594336+lpovy@users.noreply.github.com>
2022-12-29 14:05:25 +0200

    Penktadienio uzduotis
    Prekiu krepselio uzduotis

---
+++ prekiu-krepselis/java.js
@@ -0,0 +1,26 @@
+const shoppingCart = document.getElementById('shopping-cart');
+const addForm = shoppingCart.querySelector('#add-form');
+const cartItems = shoppingCart.querySelector('#cart-items');
+const totalCell = shoppingCart.querySelector('tfoot td:last-child');
+const checkoutButton = shoppingCart.querySelector('#checkout-button');
+
+let products = [];
+
+addForm.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const product = addForm.querySelector('#product').value;
+  const price = addForm.querySelector('#price').value;
+
+  addForm.reset();
+  addItem(product, price);
+});
+
+cartItems.addEventListener('input', e => {
+  const input = e.target;
+  const tr = input.closest('tr');
+  const index = tr.dataset.index;
+  const quantity = input.value;
+  const priceCell = tr.querySelector('td:nth-child(3)');
+  const price = parseFloat(priceCell.textContent.replace('$', ''));
+  const total
